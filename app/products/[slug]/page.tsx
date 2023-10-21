3a8714c1826a9a8ec8953dfe216894cf25c7989a
Talha Imran <talhaimran284@gmail.com>
2023-10-21 18:49:32 +0500

    feat(update): made product details page dynamic & responsive

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,18 +1,35 @@
 "use client";
 
 import Wrapper from 'components/Wrapper'
+import { products } from 'mock/product';
+import { Product } from 'types/product';
+import Image from 'next/image';
 import useCart from 'store/useCart';
 
-const ProductDetails = () => {
+const ProductDetails = ({
+    params
+}: {
+    params: { slug: string };
+}) => {
     const { addToCart } = useCart();
+    const { slug } = params;
+
+    const product: Product | undefined = products.find((product: Product) => product.id === parseInt(slug));
+
+    if (!product) return <Wrapper>Product Not Found!</Wrapper>
 
     return (
-        <div>
-            <Wrapper>
-                <h1>Product Details Page</h1>
-                <button onClick={addToCart.bind(this, { name: "test product", id: 123, imageUrl: "", price: 20 })}>Add to Cart</button>
-            </Wrapper>
-        </div >
+        <Wrapper>
+            <div className="flex flex-col lg:flex-row justify-between items-center gap-5 my-5">
+                <Image src={product.imageUrl} alt="Placeholder" className="flex-1 rounded-md" width={500} height={500} />
+                <div className="flex-1">
+                    <h1 className='font-bold text-2xl mb-3'>{product.name}</h1>
+                    <p className='text-sm mb-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, asperiores illo voluptatem voluptas magnam commodi totam laboriosam id a eaque libero doloribus laudantium eveniet, ea facilis placeat quasi cumque rerum!</p>
+                    <p className="text-gray-600 mb-3 text-sm">${product.price}</p>
+                    <button className='w-full bg-black text-white py-2 rounded-md text-sm font-bold' onClick={addToCart.bind(this, product)}>Add to Cart</button>
+                </div>
+            </div>
+        </Wrapper>
     )
 }
 
