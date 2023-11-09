bcd855b0aff4e5fe7072ab0aaa008f131094f82f
Talha Imran <talhaimran284@gmail.com>
2023-11-09 21:09:45 +0500

    UI: Added scroll to cart items div

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -70,56 +70,60 @@ const Cart = () => {
     <div
       className={`h-screen fixed top-0 ${
         isOpen ? '-right-0' : '-right-80'
-      }  bg-gray-100 w-80 shadow-2xl p-4 overflow-auto transition-all duration-700 ease-in-out z-50`}
+      }  bg-gray-100 w-80 shadow-2xl py-4 overflow-auto transition-all duration-700 ease-in-out z-50`}
     >
       <Button
         text='Close'
         action={() => toggleCart(!isOpen)}
-        className='font-bold text-sm w-[fit-content] px-3'
+        className='font-bold text-sm !w-[fit-content] px-3 ml-4'
       />
       {items.length > 0 ? (
         <div>
-          {items.map((item) => (
-            <div
-              key={item.id}
-              className='flex items-center p-4 bg-white gap-3 my-4 rounded-md'
-            >
-              <div>
-                <Image
-                  src={item.imageUrl}
-                  alt={item.name}
-                  width={100}
-                  height={100}
-                />
-              </div>
-              <div className='flex-1 flex flex-col text-xs font-medium gap-y-1'>
-                {item.name}
-                <div className='flex items-center justify-between'>
-                  <p>Quantity: {item.quantity}</p>
-                  <div className='flex justify-between items-center gap-1'>
-                    <div
-                      className='bg-black h-4 w-4 font-bold text-md cursor-pointer rounded-full text-white flex justify-center items-center'
-                      onClick={decreaseItemQuantity.bind(this, item)}
-                    >
-                      -
-                    </div>
-                    <div
-                      className='bg-black h-4 w-4 font-bold text-md cursor-pointer rounded-full text-white flex justify-center items-center'
-                      onClick={increaseItemQuantity.bind(this, item)}
-                    >
-                      +
+          <div className='my-4 flex flex-col gap-4 max-h-[calc(100vh-168px)] overflow-auto'>
+            {items.map((item) => (
+              <div
+                key={item.id}
+                className='flex items-center p-4 bg-white gap-3 rounded-md'
+              >
+                <div>
+                  <Image
+                    src={item.imageUrl}
+                    alt={item.name}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+                <div className='flex-1 flex flex-col text-xs font-medium gap-y-1'>
+                  {item.name}
+                  <div className='flex items-center justify-between'>
+                    <p>Quantity: {item.quantity}</p>
+                    <div className='flex justify-between items-center gap-1'>
+                      <div
+                        className='bg-black h-4 w-4 font-bold text-md cursor-pointer rounded-full text-white flex justify-center items-center'
+                        onClick={decreaseItemQuantity.bind(this, item)}
+                      >
+                        -
+                      </div>
+                      <div
+                        className='bg-black h-4 w-4 font-bold text-md cursor-pointer rounded-full text-white flex justify-center items-center'
+                        onClick={increaseItemQuantity.bind(this, item)}
+                      >
+                        +
+                      </div>
                     </div>
                   </div>
+                  <p>Price: ${item.price}</p>
                 </div>
-                <p>Price: ${item.price}</p>
               </div>
-            </div>
-          ))}
-          <p className='font-bold text-sm'>Total: ${total}</p>
-          <Button text='Checkout' action={handleCheckout} className='mt-3' />
+            ))}
+          </div>
+          <div className='absolute bottom-4 left-4 right-4'>
+            <p className='font-bold text-sm'>Total: ${total}</p>
+            <Button text='Checkout' action={handleCheckout} className='mt-3' />
+          </div>
         </div>
       ) : (
-        <p className='mt-2'>Cart is empty!</p>
+        <p className='mt-2 ml-4'>Cart is empty!</p>
       )}
     </div>
   )
